e81064c8461ec98c0e1a4ffc36814b8116bf14ed
Wixeleven <aleksejs.zukovskis@valmiera.edu.lv>
2021-03-14 20:32:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# Spēles apraksts
-# Autors
-# Datums, vieta
+# Guess your number
+# Aleksejs Žukovskis
+# 14.03.2021, Valmiera
 
 # Importē random libary
 # Importē math libary
@@ -9,16 +9,16 @@
 def game_function():
     # User input skailtim no kura(min) līdz kuram(max) tiks izvēlēts nejaušs skaitlis, kas jāuzmin
     # Ievadi attiecīgu tekstu, lai lietotājs saprot, ka lower ir min un upper max
-    lower = int(input(":- "))
-    upper = int(input(":- "))
+    lower = int(input("Enter the number from which to start the game "))
+    upper = int(input("Enter the number where the game will end (greater than the previous one) "))
 
     # Tiek nejauši izvēlēts random skaitlis no lietotāja izvēlētā apgabala, kas būs skaitlis, kas jāuzmin
     # __________ vietā ieraksti vajadzīgo library
-    x = _______.randint(lower, upper)
+    x = random.randint(lower, upper)
 
-    #Attiecīgi norādītajam apgabalam tiek izrēķināts atbilstošs mēģinājumu skaits
+    # Attiecīgi norādītajam apgabalam tiek izrēķināts atbilstošs mēģinājumu skaits
     print("\n\tTev ir tikai ", round(math.log(upper - lower + 1, 2)),
-      " mēģinājumi uzminēt skaitli!\n")
+          " mēģinājumi uzminēt skaitli!\n")
 
     # Skaitītājs minējumiem.
     count = 0
@@ -27,24 +27,30 @@ def game_function():
         count += 1
         guess = int(input("Uzmini skaitli:- "))
         # __ ievadi ar kuru mainīgo tiks salīdzināts guess
-        if __ == guess:
-            print("Apsveicu, Tu uzvarēji ar ",count, " mēģinājumiem")
+        if x == guess:
+            print("Apsveicu, Tu uzvarēji ar ", count, " mēģinājumiem")
             break
-        elif  __ > guess:
+        elif x > guess:
             print("Tavs minējums ir pārāk mazs!")
-        elif __ < guess:
+        elif x < guess:
             print("Tavs minējums ir pārāk liels!")
 
     if count >= math.log(upper - lower + 1, 2):
-        print("\nSkaitlis ir%d" % x)
+        print("\nSkaitlis ir% d" % x)
         print("\tVēlu veiksmi nākamreiz")
-    
+
     return 0
+
+
 
 def welcome_text(text):
     print(f'Hi, {text}')
     # Izsauc game_function, funkcijai nav jānorāda nekādi parametri
 
+if __name__ == '__main__':
+    game_function()
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # Izsauc welcome_text funkciju un parametros norādi izprintējamo tekstu
+    welcome_text("Do u wanna play more? ")
+# Izsauc welcome_text funkciju un parametros norādi izprintējamo tekstu
